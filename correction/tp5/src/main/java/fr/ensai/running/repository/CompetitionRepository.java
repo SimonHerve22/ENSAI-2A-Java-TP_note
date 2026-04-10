f8bb8b89ba7fbbb5bbe79ee5cc5b25a304323775
SimonHerve22 <simon.herve@eleve.ensai.fr>
2026-04-10 15:32:58 +0200

    Delete correction directory (aucun rapport avec le TP noté)

--- correction/tp5/src/main/java/fr/ensai/running/repository/CompetitionRepository.java
+++
@@ -1,8 +0,0 @@
-package fr.ensai.running.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import fr.ensai.running.model.Competition;
-
-public interface CompetitionRepository extends JpaRepository<Competition, Long> {
-}
